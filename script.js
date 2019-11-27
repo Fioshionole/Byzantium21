6ceb42d371ee82cef096e99628aa6da2a05bd0db
Fioshionole <53978487+Fioshionole@users.noreply.github.com>
2019-11-27 19:23:25 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-document.body.onload=function(){setTimeout(function(){var e=document.getElementById("page-preloader");e.classList.contains("done")||e.classList.add("done")},100)},document.querySelector("#search").oninput=function(){var e=this.value.toLowerCase(this.value.trim()),t=document.querySelectorAll(".container a");document.querySelector(".container");""!=e?t.forEach(function(t){-1==t.innerText.search(e)?t.classList.add("hide"):t.classList.remove("hide")}):t.forEach(function(e){e.classList.remove("hide")})};
+//Загрузчик
+document.body.onload = function test() {
+	setTimeout(function() {
+		var preloader = document.getElementById('page-preloader');
+		if (!preloader.classList.contains('done')) {
+			preloader.classList.add('done');
+		}
+	}, 100)
+}
+
+//Поиск
+document.querySelector('#search').oninput = function () {
+    var val = this.value.toLowerCase(this.value.trim());
+    var searchItems = document.querySelectorAll('.container a');
+    var container = document.querySelector('.container');
+
+    if (val != '') {
+        searchItems.forEach(function (elem) {
+            if (elem.innerText.search(val) == -1) {
+                elem.classList.add('hide');
+            }
+            else {
+                elem.classList.remove('hide');
+            }
+        });
+    }
+    else {
+        searchItems.forEach(function (elem) {
+            elem.classList.remove('hide');
+        });
+    }
+}
